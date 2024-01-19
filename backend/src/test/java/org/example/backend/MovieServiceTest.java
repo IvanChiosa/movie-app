4c02472db7c7af87ae4b6b8746efcf570d936abe
Ivan <i85chiosa@gmail.com>
2024-01-19 14:33:28 +0100

    Test movie

---
+++ backend/src/test/java/org/example/backend/MovieServiceTest.java
@@ -0,0 +1,11 @@
+package org.example.backend;
+
+import org.junit.jupiter.api.Test;
+
+public class MovieServiceTest {
+
+    @Test
+    void testAllMovies_whenCalled_thenReturnAllMovies() {
+
+    }
+}

782585ef8ad75673042d46efcddda38d12a0a017
Ivan <i85chiosa@gmail.com>
2024-01-23 12:36:07 +0100

    IntegrationTest Controller Update Movie

--- backend/src/test/java/org/example/backend/controller/MovieControllerTest.java
+++ backend/src/test/java/org/example/backend/controller/MovieControllerTest.java
@@ -5,6 +5,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.backend.model.Movie;
+import org.example.backend.repository.MovieRepository;
 import org.example.backend.service.MovieService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import java.util.List;
+
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -24,8 +32,6 @@ class MovieControllerTest {
 
     @Autowired // Been verkabelt container
     private MockMvc mockMvc; // das ist eine Instance von MockMvc
-
-    private MovieService movieService;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -44,21 +50,21 @@ class MovieControllerTest {
         mockMvc.perform(post("/api/v1/movies/add")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
-                        {
-                            "imdbId": "tt1234567",
-                            "title": "Test Movie",
-                            "releaseDate": "2021-01-01",
-                            "trailerLink": "https://www.youtube.com/watch?v=1234567",
-                            "poster": "https://www.imdb.com/title/tt1234567/",
-                            "genres": [
-                                "Action",
-                                "Adventure"
-                            ],
-                            "backdrops": [
-                                "https://www.imdb.com/title/tt1234567/mediaviewer/rm1234567"
-                            ]
-                        }
-                        """))
+                                {
+                                    "imdbId": "tt1234567",
+                                    "title": "Test Movie",
+                                    "releaseDate": "2021-01-01",
+                                    "trailerLink": "https://www.youtube.com/watch?v=1234567",
+                                    "poster": "https://www.imdb.com/title/tt1234567/",
+                                    "genres": [
+                                        "Action",
+                                        "Adventure"
+                                    ],
+                                    "backdrops": [
+                                        "https://www.imdb.com/title/tt1234567/mediaviewer/rm1234567"
+                                    ]
+                                }
+                                """))
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
                         {
@@ -78,14 +84,70 @@ class MovieControllerTest {
                         """));
     }
 
-
     @Test
-    void addMovie() {
+    void updateMovie_ShouldReturnUpdatedMovie_WhenMovieExists() throws Exception {
+        Movie movie = new Movie();
+        movie.setImdbId("NinaIvanovna");
+        movie.setTitle("Ivan Bello");
+        movie.setReleaseDate("2010-07-16");
+        movie.setTrailerLink("https://www.youtube.com");
+        movie.setPoster("img");
+        movie.setGenres(List.of("Natalia", "Ecaterina", "Ivan", "Nina"));
+        movie.setBackdrops(List.of("https://image.tmdb.org/t/p/original/7zQJYV02yehWrQN6NjKsBorqUUS.jpg"));
+
+
+        String movieJson = objectMapper.writeValueAsString(movie);
+        Movie movie1 = objectMapper.readValue (mockMvc.perform(post("/api/v1/movies/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(movieJson))
+                        .andReturn().getResponse().getContentAsString(), Movie.class);
+
+
+        mockMvc.perform(put("/api/v1/movies/" + movie1.getId())
+
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(movieJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Ivan Bello"))
+                .andExpect(jsonPath("$.releaseDate").value("2010-07-16"))
+                .andExpect(jsonPath("$.trailerLink").value("https://www.youtube.com"))
+                .andExpect(jsonPath("$.poster").value("img"))
+                .andExpect(jsonPath("$.genres").isArray())
+                .andExpect(jsonPath("$.genres[0]").value("Natalia"))
+                .andExpect(jsonPath("$.backdrops").isArray())
+                .andExpect(jsonPath("$.backdrops[0]").value("https://image.tmdb.org/t/p/original/7zQJYV02yehWrQN6NjKsBorqUUS.jpg"));
     }
 
-    @Test
-    void updateMovie() {
-    }
+
+
+//    @Test
+//    void updateMovie_ShouldReturnUpdatedMovie_WhenMovieExists() {
+//        Movie movie = new Movie();
+////        movie.setImdbId("tt8760708");
+//        movie.setTitle("M3GAN");
+//        movie.setReleaseDate("2023-01-06");
+//        movie.setTrailerLink("https://www.youtube.com/watch?v=BRb4U99OU80");
+//        movie.setPoster("https://image.tmdb.org/t/p/w500/xBl5AGw7HXZcv1nNXPlzGgO4Cfo.jpg");
+//        movie.setGenres(List.of("Science Fiction", "Horror", "Comedy"));
+//        movie.setBackdrops(List.of("https://image.tmdb.org/t/p/original/5kAGbi9MFAobQTVfK4kWPnIfnP0.jpg"));
+//
+//        HttpEntity<Movie> request = new HttpEntity<>(movie);
+//
+//        ResponseEntity<Movie> response = restTemplate.exchange(
+//                "/api/v1/movies/tt8760708",
+//                HttpMethod.PUT,
+//                request,
+//                Movie.class);
+//
+//        assert response.getStatusCode().is2xxSuccessful();
+////        assert "tt8760708".equals(Objects.requireNonNull(response.getBody()).getImdbId());
+//        assert "M3GAN".equals(response.getBody().getTitle());
+//        assert "2023-01-06".equals(response.getBody().getReleaseDate());
+//        assert "https://www.youtube.com/watch?v=BRb4U99OU80".equals(response.getBody().getTrailerLink());
+//        assert "https://image.tmdb.org/t/p/w500/xBl5AGw7HXZcv1nNXPlzGgO4Cfo.jpg".equals(response.getBody().getPoster());
+//        assert response.getBody().getGenres().containsAll(List.of("Science Fiction", "Horror", "Comedy"));
+//        assert response.getBody().getBackdrops().contains("https://image.tmdb.org/t/p/original/5kAGbi9MFAobQTVfK4kWPnIfnP0.jpg");
+//    }
 
     @Test
     void deleteMovie() {
